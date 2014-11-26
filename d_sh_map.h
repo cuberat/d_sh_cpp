64169b7d20c5d42bb6bdeb256b39fee646a0ac3a
Don Owens <git@regexguy.com>
2014-11-26 03:48:10 +0000

    updates

---
+++ d_sh_map.h
@@ -0,0 +1,35 @@
+/* -*- mode: c++ -*- */
+/*
+  Copyright (c) 2014 Don Owens <don@regexguy.com>.  All rights reserved.
+
+  This software is licensed under the Apache license, version 2.0,
+  available at:
+
+  http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+#ifndef D_SH_MAP_H
+#define D_SH_MAP_H
+
+#include <d_sh_cpp/d_sh_ref.h>
+
+class DSh_Map: public DSh_Obj {
+  public:
+    DSh_Map(): DSh_Obj() { this->_hash = g_hash_table_new_full(_hash_func,
+            _key_equal_func, _destroy_func, _destroy_func);}
+
+  private:
+    GHashTable *_hash;
+    
+    static gboolean _key_equal_func(gconstpointer a, gconstpointer b);
+    static void _destroy_func(gpointer data);
+    static guint _hash_func(gconstpointer key); // use g_str_hash()
+};
+
+class DSh_MapRef: public DSh_RefBase<DSh_Map> {
+  public:
+    DSh_MapRef(DSh_Map *p = 0): DSh_RefBase<DSh_Map>(p) { }
+};
+
+
+#endif
---
+++ d_sh_nil.h
@@ -0,0 +1,28 @@
+/* -*- mode: c++ -*- */
+/*
+  Copyright (c) 2014 Don Owens <don@regexguy.com>.  All rights reserved.
+
+  This software is licensed under the Apache license, version 2.0,
+  available at:
+
+  http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+#ifndef D_SH_NIL_H
+#define D_SH_NIL_H
+
+class DSh_Nil: public DSh_Obj {
+  public:
+    DSh_Nil(): DSh_Obj() { }
+    
+    virtual int is_nil() { return 1; }
+
+  private:
+};
+
+class DSh_NilRef: public DSh_RefBase<DSh_IO> {
+  public:
+    DSh_NilRef(DSh_IO *p = 0): DSh_RefBase<DSh_Nil>(p) { }
+};
+
+#endif
